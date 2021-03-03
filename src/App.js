6238a7adfabe500e36d19c0e1c7fafc8fb844add
BULENT KAYICI <bulentkyc@MacBook-Pro.fritz.box>
2021-03-03 15:36:58 +0100

    fc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import Input from './components/Input';
+let myObj = {key:'val'}
+//console.log('xyz',myObj);
+
+/* 
+let x= 123;
+let y = x;
+x = 'abc';
+console.log(x) //abc
+console.log(y) //123
+//-----------------//
+//-----------------//
+
+let x = {key:123}
+let y = x;
+x.key = 'abc';
+console.log(x) //{key:abc}
+console.log(y) //{key:abc}
+
+
+let fakeState = null
+ */
 
 function App() {
+  const [state, setState] = useState('Hello World');
+
+  let test = 'test1';
+
+  let keyHandler = (e) => {
+    //console.log(e, e.key, e.target.value);
+    //console.log(testParameter);
+    console.log(state);
+    console.log(test);
+    setState(e.target.value);
+  }
+
+  let objUpdater = (key,val) => {
+    myObj[key] = val;
+    console.log(key,val);
+    console.log('o2', myObj)
+  }
+  console.log('o', myObj)
+
+  let status = 'Text is too short!';
+    
+  if (state.length >= 5) {
+    status = 'That\'s OK!';
+  }
+
+  let letters = state.split('');
+
+  let cards = letters.map(letter => <p className="card">{letter}</p>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" className="App-header">
+      <h1>the404s React Task</h1>
+      <Input 
+        change = {keyHandler}
+        obj = {myObj}
+      >{state}</Input>
+      <p>{status}</p>
+      <p>There are {state.length} characters</p>
+      <div className="card-wrapper">{cards}</div>
+      <input placeholder = "I'm a place holder!"/>
     </div>
   );
 }
---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import {useState} from 'react';
+
+export default function (props) {
+
+    let test = 'test2';
+    props.obj.newKey = 'xyz'
+
+    //console.log('component',props.change, state);
+    return (
+        <input onChange = {props.change} value = {props.children}/>
+    );
+}
